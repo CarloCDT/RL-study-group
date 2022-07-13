95df67f6cc79f6c304f53bb1cc1b2d42a7ed305e
Carlo di Francescantonio <carlo.dt@hotmail.com>
2022-07-13 08:09:30 -0500

    Add agent DNN

--- car_racing_dev/CarRacingPlots.py
+++ car_racing_dev/CarRacingPlots.py
@@ -19,4 +19,31 @@ def plot_performance(scores, title = 'Title', window = 50):
     plt.fill_between(t_average.index, 0, t_average, color='#33cccc', alpha=0.05)
     plt.legend(loc='upper left')
     plt.title(title, fontsize=30 )
-    plt.show()
+    plt.show();
+
+def plot_episode_duraton(episode_durations, title = 'Episode Duration'):
+
+    plt.figure(figsize=(12,7))
+    sns.set_context("talk")
+
+    plt.plot(episode_durations, '#33cccc', label='Episode Duration')
+    plt.ylabel('# Time Steps')
+    plt.xlabel('Episode')
+    plt.grid(linestyle=':')
+    plt.legend(loc='upper left')
+    plt.title(title, fontsize=30 )
+    plt.show();
+
+
+def plot_epsilon(epsilons, title = 'Epsilon Values'):
+
+    plt.figure(figsize=(12,7))
+    sns.set_context("talk")
+
+    plt.plot(epsilons, '#33cccc', label='ε - value')
+    plt.ylabel('Epsilon (ε)')
+    plt.xlabel('Episode')
+    plt.grid(linestyle=':')
+    plt.legend(loc='upper right')
+    plt.title(title, fontsize=30 )
+    plt.show();
